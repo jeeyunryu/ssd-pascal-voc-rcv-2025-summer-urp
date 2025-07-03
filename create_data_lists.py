98a4d446f439a7a8f68a1542d9e8d952cdaae3f8
Jeeyun Ryu <jeeyunryu@gmail.com>
2025-07-03 23:10:03 +0000

    init commit

---
+++ create_data_lists.py
@@ -0,0 +1,6 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    create_data_lists(voc07_path='/home/jyryu/workspace/datasets/pascal_voc/VOC2007',
+                      voc12_path='/home/jyryu/workspace/datasets/pascal_voc/VOC2012',
+                      output_folder='./')
